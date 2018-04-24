0b57830cdf77a47afe6f65d61597f0b22e560c9a
vchapple17 <vchapple17@gmail.com>
2018-04-24 10:16:07 -0500

    Check that Location Providers are available. Send user to provider if not.

--- app/src/main/java/com/home_turf/location_spike/MapsActivity.java
+++ app/src/main/java/com/home_turf/location_spike/MapsActivity.java
@@ -2,8 +2,11 @@ package com.home_turf.location_spike;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Looper;
 import android.support.annotation.NonNull;
@@ -12,14 +15,15 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
 import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -27,17 +31,28 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 import java.util.ArrayList;
+import java.util.List;
 
+// Save State
+// https://developer.android.com/topic/libraries/architecture/saving-states.html
+
+// https://developer.android.com/training/location/change-location-settings.html
 
 // https://github.com/googlemaps/android-samples/blob/master/tutorials/CurrentPlaceDetailsOnMap/app/src/main/java/com/example/currentplacedetailsonmap/MapsActivityCurrentPlace.java
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback,
         ActivityCompat.OnRequestPermissionsResultCallback,
         GoogleMap.OnMyLocationButtonClickListener,
         GoogleMap.OnMyLocationClickListener {
+
     private static final String TAG = MapsActivity.class.getSimpleName();
     private static final int LOCATION_PERMISSION_RESULT = 17;
+    private static final int REQUEST_CHECK_SETTINGS = 1234;
     private static final double DEFAULT_LAT = 44.5;
     private static final double DEFAULT_LON = -123.2;
 
@@ -45,8 +60,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     private GoogleMap mMap;
     private View mLayout;
     private boolean isMapReady;
-
-
     private String mLongitude;
     private String mLatitude;
 
@@ -61,50 +74,39 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     protected final static String REQUESTING_LOCATION_UPDATES_KEY = "requesting-location-updates-key";
 //    protected final static String LOCATION_KEY = "location-key";
 
+
     // Location Services API
     private LocationRequest mLocationRequest;
-    private long UPDATE_INTERVAL = 10 * 1000;  /* 10 secs */
+    private long UPDATE_INTERVAL = 5 * 1000;  /* 5 secs */
     private long FASTEST_INTERVAL = 2000; /* 2 sec */
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Basic setup from bundle
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-
         updateValuesFromBundle(savedInstanceState);
+        mLayout = findViewById(R.id.map_layout);
 
+        if (checkProviders()) {
+            Log.d(TAG, "checkProviders is true");
+        }
+        else {
+            Log.d(TAG, "checkProviders is false");
+        }
+
+        // Location Client
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+        // Load Map Fragment Asynchronously. Get notified via onMapReady function
+        isMapReady = false;
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        mLongitude = getIntent().getStringExtra("Lat");
-        mLatitude = getIntent().getStringExtra("Lon");
-        mCurrentLocation = new Location("Default");
-        mCurrentLocation.setLatitude(Double.valueOf(mLatitude));
-        mCurrentLocation.setLongitude(Double.valueOf(mLongitude));
-        isMapReady = false;
 
-
-        // Location Request
-        // https://github.com/codepath/android_guides/wiki/Retrieving-Location-with-LocationServices-API
-        // Create the location request to start receiving updates
-        mLocationRequest = new LocationRequest();
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        mLocationRequest.setInterval(UPDATE_INTERVAL);
-        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
-
-        // Create LocationSettingsRequest object using location request
-        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
-        builder.addLocationRequest(mLocationRequest);
-        LocationSettingsRequest locationSettingsRequest = builder.build();
-
-        // Check whether location settings are satisfied
-        // https://developers.google.com/android/reference/com/google/android/gms/location/SettingsClient
-        SettingsClient settingsClient = LocationServices.getSettingsClient(this);
-        settingsClient.checkLocationSettings(locationSettingsRequest);
-
+        // Create Location Request and Check Phone Settings
+        createLocationRequest();
 
         // Location Callback
         mLocationCallback = new LocationCallback() {
@@ -130,8 +132,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             Log.d(TAG, "onCreate startLocationUpdates.");
             startLocationUpdates();
         }
-
-
     }
 
     @Override
@@ -159,6 +159,70 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 //        updateUI();
     }
 
+    private void createLocationRequest() {
+        // Location Request object for Updating location
+        // https://github.com/codepath/android_guides/wiki/Retrieving-Location-with-LocationServices-API
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+
+        // LocationSettingsRequest object to check that location settings of phone are ready.
+        // https://developer.android.com/training/location/change-location-settings.html#java
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(mLocationRequest);
+        LocationSettingsRequest locationSettingsRequest = builder.build();
+        SettingsClient settingsClient = LocationServices.getSettingsClient(this);
+        Task<LocationSettingsResponse> task = settingsClient.checkLocationSettings(locationSettingsRequest);
+        // If location settings are good...
+        task.addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                // All location settings are satisfied. The client can initialize
+                // location requests here.
+                // ...
+            }
+        });
+        // If location settings are not good...
+        task.addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if (e instanceof ResolvableApiException) {
+                    // Location settings are not satisfied, but this can be fixed
+                    // by showing the user a dialog.
+                    try {
+                        // Show the dialog by calling startResolutionForResult(),
+                        // and check the result in onActivityResult().
+                        ResolvableApiException resolvable = (ResolvableApiException) e;
+                        resolvable.startResolutionForResult(MapsActivity.this,
+                                REQUEST_CHECK_SETTINGS);
+                    } catch (IntentSender.SendIntentException sendEx) {
+                        // Ignore the error.
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CHECK_SETTINGS) {    //
+            // Make sure the request was successful
+            switch (resultCode) {
+                case RESULT_OK:
+                    Log.i(TAG, "User agreed to make required location settings changes.");
+                    // Nothing to do. startLocationupdates() gets called in onResume again.
+                    break;
+                case RESULT_CANCELED:
+                    Log.i(TAG, "User chose not to make required location settings changes.");
+                    mRequestingLocationUpdates = false;
+                    Snackbar.make(mLayout, "Location Services are Required",Snackbar.LENGTH_LONG);
+                    mMap.clear();
+                    break;
+            }
+            return;
+        }
+    }
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -182,19 +246,36 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onResume() {
         super.onResume();
-        if (mRequestingLocationUpdates) {
-            startLocationUpdates();
-        }
-//        // Check location permissions
-//        if (checkPermissions() == false) {
-//            // Ask for permission
-//            Log.d(TAG, "onResume Location Denied.");
-//            requestLocationPermission();
-//        } else {
-//            // Get location
-//            Log.d(TAG, "onResume startLocationUpdates.");
+
+//        if (mRequestingLocationUpdates) {
 //            startLocationUpdates();
 //        }
+        // Check location permissions
+        if (checkPermissions() == false) {
+            // Ask for permission
+            Log.d(TAG, "onResume Location Denied.");
+            requestLocationPermission();
+        } else {
+            if (checkProviders()) {
+                // Get location
+                Log.d(TAG, "onResume startLocationUpdates.");
+                startLocationUpdates();
+            }
+            else {
+                Log.d(TAG, "onResume noProviders available.");
+                startLocationUpdates();
+                Snackbar.make(mLayout, R.string.locationPermissionRequired,
+                        Snackbar.LENGTH_INDEFINITE).setAction(R.string.okButtonText, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // Request the permission
+                        Log.d(TAG, "requestLocationPermission OK to request Location Permission.");
+//                        startLocationPermissionRequest();
+                        startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                }).show();
+            }
+        }
     }
 
     private void requestLocationPermission() {
@@ -253,6 +334,22 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
+    private boolean checkProviders() {
+        LocationManager locationManager = (LocationManager) getApplicationContext()
+                .getSystemService(LOCATION_SERVICE);
+        List<String> providers = locationManager.getProviders(true);
+        boolean isGpsEnabled = false;
+        boolean isNetworkEnabled = false;
+        for (String s: providers) {
+            if (s.equals("gps")) { isGpsEnabled = true; }
+            else if (s.equals("network")) { isNetworkEnabled = true; }
+        }
+
+        if (isGpsEnabled && isNetworkEnabled) { return true; }
+        else { return false; }
+
+    }
+
     @SuppressLint("MissingPermission")
     private void onLocationChanged(Location location) {
         if (isMapReady) {
@@ -270,7 +367,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             }
         }
     }
-    
+
     private void addGamePins() {
         // Mock game
         ArrayList<Double> latitudes = new ArrayList<>();
@@ -327,5 +424,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     private void stopLocationUpdates() {
         mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+        mRequestingLocationUpdates = false;
     }
 }
